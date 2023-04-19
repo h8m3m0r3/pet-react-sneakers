081fbd2259bd626a5e7e87d971b957f3b7909b01
h8m3m0r3 <danilayumatov@yandex.ru>
2023-04-19 15:52:11 +0300

    react

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
-import Card from "./components/Card";
+import React from "react";
+import Card from "./components/Card/Card";
 import Header from "./components/Header";
 import Overlay from "./components/Overlay";
 
 function App() {
+  const [items, setItems] = React.useState([]);
+  const [cartItems, setCartItems] = React.useState([]);
+  const [cartOpened, setCartOpened] = React.useState(false);
+
+  React.useEffect(() => {
+    fetch("https://643ebfc66c30feced83228c7.mockapi.io/items")
+      .then((res) => {
+        return res.json();
+      })
+      .then((json) => {
+        setItems(json);
+      });
+  }, []);
+
+  const onAddToCart = (obj) => {
+    setCartItems(prev => [...prev, obj]);
+  };
+
   return (
     <div className="wrapper clear">
-      <Overlay />
-      <Header />
+      {cartOpened && (
+        <Overlay items={cartItems} onClose={() => setCartOpened(false)} />
+      )}
+      <Header onClickCart={() => setCartOpened(true)} />
       <div className="content p-40">
         <div className="d-flex align-center justify-between mb  -40">
           <h1>Все кроссовки</h1>
@@ -15,8 +36,16 @@ function App() {
             <input placeholder="Поиск... " />
           </div>
         </div>
-        <div className="d-flex">
-          <Card />
+        <div className="d-flex flex-wrap">
+          {items.map((item) => (
+            <Card
+              imageUrl={item.imageUrl}
+              title={item.title}
+              price={item.price}
+              onFavorite={() => console.log("Добавлено в избранное")}
+              onPlus={(obj) => onAddToCart(obj)}
+            />
+          ))}
         </div>
       </div>
     </div>
--- src/components/Overlay.jsx
+++ src/components/Overlay.jsx
@@ -1,22 +1,28 @@
-import React from 'react';
+import React from "react";
 
-const Overlay = () => {
-    return (
-        <div style={{ display: "none" }} className="overlay">
-        <div className="drawer">
-          <h2 className="d-flex justify-between mb-30">
-            Корзина
-            <img className="cu-p" src="/img/btn-remove.svg" alt="Remove" />
-          </h2>
-          <div className="items">
+const Overlay = ({ onClose, items = [] }) => {
+  return (
+    <div className="overlay">
+      <div className="drawer">
+        <h2 className="d-flex justify-between mb-30">
+          Корзина
+          <img
+            className="cu-p"
+            src="/img/btn-remove.svg"
+            alt="Close"
+            onClick={onClose}
+          />
+        </h2>
+        <div className="items">
+          {items.map((obj) => (
             <div className="cart_item d-flex align-center mb-20">
               <div
                 className="cart_item_img"
-                style={{ backgroundImage: "url(/img/sneakers/1.jpg)" }}
+                style={{ backgroundImage: `url(${obj.imageUrl})` }}
               ></div>
               <div className="mr-20 flex">
-                <p className="mb-5">Мужские Кроссовки Nike Blazer Mid Suede</p>
-                <b>12 999 руб.</b>
+                <p className="mb-5">{obj.title}</p>
+                <b>{obj.price}</b>
               </div>
               <img
                 className="remove_btn"
@@ -24,42 +30,28 @@ const Overlay = () => {
                 alt="Remove"
               />
             </div>
-            <div className="cart_item d-flex align-center mb-20">
-              <div
-                className="cart_item_img"
-                style={{ backgroundImage: "url(/img/sneakers/1.jpg)" }}
-              ></div>
-              <div className="mr-20 flex">
-                <p className="mb-5">Мужские Кроссовки Nike Blazer Mid Suede</p>
-                <b>12 999 руб.</b>
-              </div>
-              <img
-                className="remove_btn"
-                src="/img/btn-remove.svg"
-                alt="Remove"
-              />
-            </div>
-          </div>
-          <div className="cart_total_block">
-            <ul>
-              <li>
-                <span>Итого:</span>
-                <div></div>
-                <b>21 498 руб.</b>
-              </li>
-              <li>
-                <span>Налог 5%:</span>
-                <div></div>
-                <b>1074 руб.</b>
-              </li>
-            </ul>
-            <button className="greenButton">
-              Оформить заказ <img src="/img/arrow.svg" alt="Arrow" />
-            </button>
-          </div>
+          ))}
+        </div>
+        <div className="cart_total_block">
+          <ul>
+            <li>
+              <span>Итого:</span>
+              <div></div>
+              <b>21 498 руб.</b>
+            </li>
+            <li>
+              <span>Налог 5%:</span>
+              <div></div>
+              <b>1074 руб.</b>
+            </li>
+          </ul>
+          <button className="greenButton">
+            Оформить заказ <img src="/img/arrow.svg" alt="Arrow" />
+          </button>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Overlay;
